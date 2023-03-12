8856065a43d1b5bc42816c87ccf4aab7fcb91f27
nikita <nikitapr@student.ubc.ca>
2023-03-12 00:21:33 -0800

    PHASE 2 added tests

--- src/test/model/LogEntryTest.java
+++ src/test/model/LogEntryTest.java
@@ -52,6 +52,15 @@ class LogEntryTest {
         assertNotEquals("Salary", "Food");
         assertEquals("Food",l2.getCategory());
     }
+    @Test
+    public void resetThenChangeTest() {
+        l2.setCategory("Salary");
+        assertEquals("Salary",l2.getCategory());
+        l2.changeCategory("Salary");
+        assertEquals("Not Categorized",l2.getCategory());
+        l2.setCategory("Grocery");
+        assertEquals("Grocery",l2.getCategory());
+    }
 
     @Test
     public void getDateTest() {
